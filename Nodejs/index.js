01a49f68ee435c0f697e1c3731af7f116b3e1fdf
monika <monika18092012@gmail.com>
2025-08-20 10:46:39 +0530

    nodejs2

---
+++ Nodejs/index.js
@@ -0,0 +1,38 @@
+// var a=12;
+// console.log("hello="+a);
+// var a=30;
+// console.log(typeof(a));
+// var a=10;
+// var a=20;
+// console.log(a);
+// -----so we have to restrict it var
+// let a=12;
+// if(a>10){
+//     let a=30;
+//     console.log("a="+a);
+// }
+// console.log(a);
+
+// ----to check equality
+// let a=12;
+// let b="12";
+// if(a===b){
+//     console.log("hii....helllo");
+// }
+// else{
+//     console.log("welcome to js");
+// }
+
+// ...function
+// function sum(){
+//     return 20+30;
+// }
+// let val=sum();
+// console.log("sum="+val);
+// function sum(a,b){
+//     return a+b;
+// }
+// let val=sum(23,12);
+// console.log("sum="+val);
+
+
