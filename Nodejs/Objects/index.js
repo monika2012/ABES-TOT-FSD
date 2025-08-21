8fd58be0b34f3464b40ec29705e90119895fe07b
monika <monika18092012@gmail.com>
2025-08-21 11:07:12 +0530

    assignmentnew

---
+++ Nodejs/Objects/index.js
@@ -0,0 +1,22 @@
+const employee={
+    ename:"Monika Singh",
+    eage:"34",
+    eaddress:"Delhi",
+    planguage:[{
+        lang1:"java",
+        lang2:"paython"
+    },
+    {
+        
+            database1:"Oracle",
+            database2:"java"
+        
+    },
+    {
+        f1:"hibernate",
+        f2:"Spring"
+    }
+]
+}
+//console.log(employee);
+console.log(employee.planguage[2].f1);
